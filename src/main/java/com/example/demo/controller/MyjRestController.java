b1c25a1482490887a43cb17f189502addf2b8985
MYJ <KTDS@host.docker.internal>
2024-10-04 16:21:10 +0900

    설정 파일 변경 및 신규 파일 추가

---
+++ src/main/java/com/example/demo/controller/MyjRestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/api/v1")
+@RestController
+public class MyjRestController {
+
+	@GetMapping("/user")
+    public String home() {
+		log.info("사번 리턴");
+        return "82238157";
+    }
+}
